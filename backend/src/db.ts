0c66d825a955bf5587e4e70a1fde54debe40df45
VangavarapuAnitha <sscanitha123@gmail.com>
2025-11-23 11:20:35 +0530

    added neon connection string

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,10 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 //Connection pool
+// export const pool = new Pool({
+//   host: process.env.DATABASE_HOST,
+//   port: Number(process.env.DATABASE_PORT),
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE_NAME,
+// });
+
 export const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  connectionString: process.env.PG_URL,
 });
