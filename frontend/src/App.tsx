ad03fb84a18627243ad0bf29cdf432fb543318a7
VangavarapuAnitha <sscanitha123@gmail.com>
2025-11-23 12:43:46 +0530

    Added Route in the App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Stats from "./pages/Stats";
+import RedirectHandler from "./pages/RedirectHandler/RedirectHandler";
 function App() {
   return (
     <div>
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/code/:code" element={<Stats />} />
+          <Route path="/:code" element={<RedirectHandler />} />
         </Routes>
       </BrowserRouter>
     </div>
